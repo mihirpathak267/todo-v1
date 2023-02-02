ef5e6ff4901a70ed4ce1158cf7d17d4f505fadf0
Mihir Pathak <mihirpathak267@gmail.com>
2023-02-02 14:20:36 +0530

    Use toLocaleDateString with options to show date.

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ app.set("view engine", "ejs");
 var currentDay = new Date();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 app.get("/", function(req, res){
-    var today = days[currentDay.getDay()]; 
-    res.render("index", {today: today});
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    // var today = days[currentDay.getDay()]; 
+    var day = currentDay.toLocaleDateString("en-US", options);
+    res.render("index", {today: day});
 });
 
 
