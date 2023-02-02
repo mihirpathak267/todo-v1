9890b0aae5ee6298a7c9402ab5666e63184221db
Mihir Pathak <mihirpathak267@gmail.com>
2023-02-02 18:17:30 +0530

    make new items on the todo list
    using <% tags in the index.ejs to render data from the app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
 const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = ["buy food", "cook food", "eat food"];
 var currentDay = new Date();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 app.get("/", function(req, res){
@@ -16,7 +20,14 @@ app.get("/", function(req, res){
     }
     // var today = days[currentDay.getDay()]; 
     var day = currentDay.toLocaleDateString("en-US", options);
-    res.render("index", {today: day});
+    res.render("index", {today: day, newItems:items});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
 });
 
 
